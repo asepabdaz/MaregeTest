544e943f68d6a35b54b7ae8612753d0d194b8636
Asepabdaz <asepabdaz@gmail.com>
2019-08-21 15:46:49 +0700

    Initial Commit

---
+++ MaregeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MaregeTest
+//
+//  Created by Hai on 21/08/19.
+//  Copyright © 2019 Asep Abdaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
