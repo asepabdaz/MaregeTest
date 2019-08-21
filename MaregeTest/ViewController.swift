742931a39f69b335d45856ebdb9e1728e105a8df
Asepabdaz <asepabdaz@gmail.com>
2019-08-21 16:21:28 +0700

    test

--- MaregeTest/ViewController.swift
+++ MaregeTest/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hai")
+        print("hai")
         // Do any additional setup after loading the view.
     }
 
